eabe8168194e42784d7e59669a20fcc4ece2c5aa
mauro <maurx18@gmail.com>
2026-03-12 12:19:20 +0100

    implements feedback on BfsSolver

--- src/main/java/ch/heig/gre/groupA/BfsSolver.java
+++ src/main/java/ch/heig/gre/groupA/BfsSolver.java
@@ -8,9 +8,9 @@ import ch.heig.gre.graph.VertexLabelling;
 import ch.heig.gre.maze.MazeSolver;
 import ch.heig.gre.maze.Metadata;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 
 public final class BfsSolver implements MazeSolver {
     @Override
@@ -20,14 +20,14 @@ public final class BfsSolver implements MazeSolver {
     }
 
     private class BfsResult { // classe interne pour le retour de la méthode BFS
-        int[] d;
-        Integer[] p;
-        int[] N;
+        int[] distanceToSource;
+        int[] parent;
+        int[] optimalPaths;
 
-        BfsResult(int[] d, Integer[] p, int[] N) {
-            this.d = d;
-            this.p = p;
-            this.N = N;
+        BfsResult(int[] distanceToSource, int[] parent, int[] optimalPaths) {
+            this.distanceToSource = distanceToSource;
+            this.parent = parent;
+            this.optimalPaths = optimalPaths;
         }
     }
 
@@ -35,21 +35,21 @@ public final class BfsSolver implements MazeSolver {
         BfsResult result = BFS(graph, source, destination, distances);                                              //calcul des distances, parents et nombre de chemins optimaux
 
         Metadata metadata = new Metadata();                                                                         // stockage des métadonnées
-        metadata.put(Keys.LENGTH, result.d[destination]);
-        metadata.put(Keys.NB_OPTIMAL_PATHS, (long) result.N[destination]);
+        metadata.put(Keys.LENGTH, result.distanceToSource[destination]);
+        metadata.put(Keys.NB_OPTIMAL_PATHS, (long) result.optimalPaths[destination]);
 
 
-        if (result.p[destination] == null) return new Result(new ArrayList<Integer>(), metadata);                   // cas ou aucun chemin a été trouvé
+        if (result.parent[destination] == -1) return new Result(new ArrayList<Integer>(), metadata);                   // cas ou aucun chemin a été trouvé
 
 
         //preparation du chemin à retourner
-        ArrayList<Integer> path = new ArrayList<>(Collections.nCopies(result.d[destination] + 1, null));      // declaration du chemin à retourner dans un tableau avec la bonne taille
-        path.set(result.d[destination],destination);                                                                // on met la destination à la fin du chemin
+        ArrayList<Integer> path = new ArrayList<>(Collections.nCopies(result.distanceToSource[destination] + 1, null));      // declaration du chemin à retourner dans un tableau avec la bonne taille
+        path.set(result.distanceToSource[destination],destination);                                                                // on met la destination à la fin du chemin
         int i = 1;                                                                                                  // index pour savoir ou inserer les sommets
-        int to = result.p[destination];                                                                             // on prend le parent de la destination
+        int to = result.parent[destination];                                                                             // on prend le parent de la destination
         while(to != source){                                                                                        // tant que le parent n'est pas la source on l'ajoute a la fin de la liste
-            path.set(result.d[destination] - i, to);
-            to = result.p[to];
+            path.set(result.distanceToSource[destination] - i, to);
+            to = result.parent[to];
             i++;
         }
         path.set(0,source);                                                                                         // on ajoute la source au début du chemin
@@ -58,43 +58,41 @@ public final class BfsSolver implements MazeSolver {
     }
 
     private BfsResult BFS(Graph graph, int source, int destination, VertexLabelling<Integer> distances) {
-        int[] d = new int[graph.nbVertices()];          //distance avec le sommet source, -1 si non encore trouvé
-        Integer[] p = new Integer[graph.nbVertices()];  //parant du sommet (celui qui a permis de le découvrir), null si non encore trouvé
-        int[] N = new int[graph.nbVertices()];          //nombre de chemins optimaux pour attendre le sommet
+        int[] distanceToSource = new int[graph.nbVertices()];          //distance avec le sommet source, -1 si non encore trouvé
+        int[] parent = new int[graph.nbVertices()];                    //parant du sommet (celui qui a permis de le découvrir), null si non encore trouvé
+        int[] optimalPaths = new int[graph.nbVertices()];              //nombre de chemins optimaux pour attendre le sommet
 
         for (int i = 0; i < graph.nbVertices(); ++i) {  //initialisation des valeurs par défaut
-            d[i] = -1;
-            p[i] = null;
-            N[i] = 0;
+            distanceToSource[i] = -1;
+            parent[i] = -1;
+            optimalPaths[i] = 0;
         }
         // initialisation des valeurs de la source
-        d[source] = 0;
-        N[source] = 1;
+        distanceToSource[source] = 0;
+        optimalPaths[source] = 1;
         distances.setLabel(source, 0);
         // file d'attente pour le parcours en largeur
-        ArrayList<Integer> Q = new ArrayList<Integer>(Collections.nCopies(graph.nbVertices(), null));
-        int in = 0;             // pseudo pointeur vers la prochaine position d'insertion dans la file
-        int out = 0;            // pseudo pointeur vers la prochaine position de lecture dans la file
+        ArrayDeque<Integer> Q = new ArrayDeque<Integer>(graph.nbVertices());
+        Q.addLast(source);    // insertion de la source dans la file
 
-        Q.set(in++, source);    // insertion de la source dans la file
-        while (in != out) {     // si la queue n'est pas vide
-            int u = Q.get(out++);   // enlever le prochain sommet de la file
+        while (!Q.isEmpty()) {     // si la queue n'est pas vide
+            int u = Q.removeFirst();   // enlever le prochain sommet de la file
             if (u == destination) { // si c'est la destination on sort de la boucle
                 break;
             }
             // pour tous ses voisins on regarde s'ils ont déjà été découverts ou pas
             for (int v : graph.neighbors(u)) {
-                if (d[v] == -1) { // decouverte, on met à jour les valeurs et on l'ajoute à la file
-                    d[v] = d[u] + 1;
-                    p[v] = u;
-                    N[v] = N[u];
-                    distances.setLabel(v, d[v]);
-                    Q.set(in++, v);
-                } else if (d[v] == d[u] + 1) { //parcours alternatif
-                    N[v] += N[u]; // on ajoute nos chemins optimaux à ceux déjà trouvés pour ce sommet
+                if (distanceToSource[v] == -1) { // decouverte, on met à jour les valeurs et on l'ajoute à la file
+                    distanceToSource[v] = distanceToSource[u] + 1;
+                    parent[v] = u;
+                    optimalPaths[v] = optimalPaths[u];
+                    distances.setLabel(v, distanceToSource[v]);
+                    Q.addLast(v);
+                } else if (distanceToSource[v] == distanceToSource[u] + 1) { //parcours alternatif
+                    optimalPaths[v] += optimalPaths[u]; // on ajoute nos chemins optimaux à ceux déjà trouvés pour ce sommet
                 }
             }
         }
-        return new BfsResult(d, p, N);
+        return new BfsResult(distanceToSource, parent, optimalPaths);
     }
 }
